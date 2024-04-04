e310be4c801de10664aa222e3d3fdebdc0732394
ferelie <abdallah.yasser0501@gmail.com>
2024-04-04 22:46:11 +0200

    display shuffled ans, save correct ans, show endgame

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+import quizLogo from '../assets/quiz-logo.png';
+
+
+const Header = () => {
+  return (
+    <header>
+        <img  src={quizLogo} alt="quiz logo" />
+        <h1>Trivia Quiz</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,51 @@
+import QUESTIONS from "../assets/questions";
+import { useState } from "react";
+import quizComplete from "../assets/quiz-complete.png";
+
+const correctAnswers = QUESTIONS.map((question) => question.answers[0]);
+
+const Quiz = () => {
+    const [playerAnswers, setPlayerAnswers] = useState([]);
+    const currentQuestion = playerAnswers.length;
+
+    const handlePlayerSelection = (answer) => {
+        setPlayerAnswers([...playerAnswers, answer]);
+        console.log(playerAnswers);
+    };
+
+    if (playerAnswers.length === QUESTIONS.length) {
+        return (
+            <div id="summary">
+                <img src={quizComplete} alt="trophy picture" />
+                
+                <h2>Thank you for playing!</h2>
+            </div>
+        );
+    }
+
+    const shuffledAnswers = QUESTIONS[currentQuestion].answers.sort(
+        () => Math.random() - 0.5
+    );
+
+    return (
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[currentQuestion].text}</h2>
+                <ul id="answers">
+                    {shuffledAnswers.map((answer) => (
+                        <li key={answer} className="answer">
+                            <button
+                                onClick={() => handlePlayerSelection(answer)}
+                            >
+                                {answer}
+                            </button>
+                        </li>
+                    ))}
+                    {/* <div>{correctAnswers[currentQuestion]}</div> */}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Quiz;
